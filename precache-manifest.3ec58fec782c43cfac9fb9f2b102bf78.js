f31e4b04259978e7722f01d2b56b15891a3568bb
Brendon Pagano <brendonpagano@gmail.com>
2018-10-22 04:12:14 -0300

    Updates

--- precache-manifest.3ec58fec782c43cfac9fb9f2b102bf78.js
+++ precache-manifest.3ec58fec782c43cfac9fb9f2b102bf78.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "900dd97f1522bb8479b0",
-    "url": "/static/js/main.900dd97f.chunk.js"
+    "revision": "d1643af02b7519742520",
+    "url": "/static/js/main.d1643af0.chunk.js"
   },
   {
     "revision": "95c23cb979264e92ca30",
     "url": "/static/js/1.95c23cb9.chunk.js"
   },
   {
-    "revision": "575d06968f55600ccca5f3082ddb4ccb",
+    "revision": "e0e7d7d50b70a3ae03d87adab38d300e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8edc56e1f2d680ba0d558b6cbde31c4.js"
+  "/precache-manifest.3ec58fec782c43cfac9fb9f2b102bf78.js"
 );
 
 workbox.clientsClaim();
